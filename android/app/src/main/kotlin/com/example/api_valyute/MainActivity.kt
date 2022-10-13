190fbe7c473e846c1f45dca1a6d960dcde0706c1
unknown <sunnatillo0107@gmail.com>
2022-10-13 07:39:56 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/api_valyute/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.api_valyute
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/card_coin.dart
@@ -0,0 +1,150 @@
+  import 'package:flutter/material.dart';
+class  CoinCard extends StatefulWidget {
+   String? name;
+  String ?symbol;
+  String? imageUrl;
+  double? price;
+  double? change;
+  double? changePercentage;
+    CoinCard({
+    required this.name,
+    required this.symbol,
+    required this.imageUrl,
+    required this.price,
+    required this.change,
+    required this.changePercentage,
+  });
+
+  @override
+  State<CoinCard> createState() => _CoinCardState();
+}
+
+class _CoinCardState extends State<CoinCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 15, left: 10, right: 10),
+      child: Container(
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade500,
+              offset: const Offset(4, 4),
+              blurRadius: 10,
+              spreadRadius: 1,
+            ),
+           const  BoxShadow(
+              color: Colors.white,
+              offset: Offset(-4, -4),
+              blurRadius: 10,
+              spreadRadius: 1,
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade500,
+                      offset:const Offset(4, 4),
+                      blurRadius: 10,
+                      spreadRadius: 1,
+                    ),
+                     const BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(-4, -4),
+                      blurRadius: 10,
+                      spreadRadius: 1,
+                    ),
+                  ],
+                ),
+                height: 60,
+                width: 60,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Image.network('${widget.imageUrl}'),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        '${widget.name}',
+                        style: TextStyle(
+                          color: Colors.grey[900],
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '${widget.symbol}',
+                      style: TextStyle(
+                        color: Colors.grey[900],
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    '${widget.price}',
+                    style: TextStyle(
+                      color: Colors.grey[900],
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    widget.change!.toDouble() < 0
+                        ? widget.change!.toDouble().toString()
+                        : '+' + widget.change!.toDouble().toString(),
+                    style: TextStyle(
+                      color: widget.change!.toDouble() < 0 ? Colors.red : Colors.green,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    widget.changePercentage!.toDouble() < 0
+                        ? widget.changePercentage!.toDouble().toString() + '%'
+                        : '+' + widget.changePercentage!.toDouble().toString() + '%',
+                    style: TextStyle(
+                      color: widget.changePercentage!.toDouble() < 0
+                          ? Colors.red
+                          : Colors.green,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
